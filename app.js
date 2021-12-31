e235da1874154f385086bf99431805a346f89840
hassanrobleh <hassan.alirobleh@gmail.com>
2021-12-31 01:42:02 +0100

    add file helper

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+// import express from 'express'
 const express = require('express')
-// import express from 'express'
-// import express from 'express'
+// import pokemons from './mock-pokemon'
+const pokemons = require('./mock-pokemon')
+const {success} = require('./helper')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello Express'))
+
 app.get('/api/pokemons/:id', (req, res) => {
-    const id = req.params.id 
-    res.send(`Vous avez demande le pokemon n° ${id}`)
+    // console.log(pokemons)
+    // res.send(`Vous avez demande le pokemon n° ${pokemon.name}`)
+
+    const id = parseInt(req.params.id )
+    const pokemon = pokemons.find((pokemon) => pokemon.id === id)  
+    const message = 'Un pokemon a bie été trouvé' 
+    res.json(success(message, pokemon))
+})
+
+// app.get('/api/pokemons', (req, res) => {
+//     const count = pokemons.length
+//     res.send(`Il y a ${count} pokemons dans le pokédox pour le moment`)
+// })
+
+app.get('/api/pokemons', (req, res) => {
+    const message = 'La liste des pokemons a bien été récupérée.' 
+    res.json(success(message, pokemons))
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
---
+++ helper.js
@@ -0,0 +1,7 @@
+
+exports.success = (message, data) => {
+    // Avec le raccourci de syntaxe ECMAScript6
+    return {message, data}
+}
+
+exports.success
