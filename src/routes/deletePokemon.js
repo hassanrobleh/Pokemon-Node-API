00e57e700d092919807fe36d8d744642b2c4ad8f
hassanrobleh <hassan.alirobleh@gmail.com>
2022-01-13 22:22:25 +0100

    fix bug app

--- src/routes/deletePokemon.js
+++ src/routes/deletePokemon.js
@@ -1,15 +1,16 @@
 import { Pokemon } from "../db/sequelize.js";
 
 const deledePokemon = (app) => {
-    app.delete('/api/pokemons/', (req, res) => {
-        Pokemon.findByPK(req.params.id).then(pokemon => {
+    app.delete('/api/pokemons/:id', (req, res) => {
+        Pokemon.findByPk(req.params.id).then(pokemon => {
+            const pokemonDeleted = pokemon;
             Pokemon.destroy({
-                where: {id: pokemon.id}
+                where: { id: pokemon.id }
             })
-        })
-        .then(_ => {
-            const message = `Le pokemon avec l'identification n°${pokemon.id} a bien été supprimé.`
-            res.json({message, data: pokemon})
+            .then(_ => {
+                const message = `Le pokémon avec l'identifiant n°${pokemonDeleted.id} a bien été supprimé.`
+                res.json({message, data: pokemonDeleted })
+            })
         })
     })
 }
