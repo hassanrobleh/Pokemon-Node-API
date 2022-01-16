ea10ec1b8f3e9e435dedc7d66e5581da2b024fdf
hassanrobleh <hassan.alirobleh@gmail.com>
2022-01-16 23:34:20 +0100

    add search with squelize

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,10 +1,19 @@
 import { Pokemon } from '../db/sequelize.js'
+import { Op } from 'sequelize'
 
 const findAllPokemons = (app) => {
     app.get('/api/pokemons', (req, res) => {
         if(req.query.name) {
             const name = req.query.name
-            return Pokemon.findAll({ where : {name: name}})
+            // return Pokemon.findAll({ where : {name: name}})
+            return Pokemon.findAll({ 
+                where: { 
+                    name: { // 'name' est la propriété du modèle pokémon
+                        //[Op.eq]: name // 'name' est le critère de la recherche
+                        [Op.like]: `%${name}%` // 'name' est le critère de la recherche
+                    }
+                }
+            })
             .then(pokemons => {
                 const message = ` Il y a ${pokemons.length} pokémons qui correspondent au terme de recherche ${name}`
                 res.json({message, data: pokemons})
