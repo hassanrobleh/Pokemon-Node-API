4d74849ebccbd175a5be9afa86dd58a1fad03fe4
hassanrobleh <hassan.alirobleh@gmail.com>
2022-01-18 00:29:09 +0100

    add error with login

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,15 +3,24 @@ import bcrypt from 'bcrypt'
 
 const login = (app) => {
     app.post('/api/login', (req, res) => {
-        User.findOne({ where: {username : req.body.username}})
-            .then(user => {
-                bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
-                    if(isPasswordValid) {
-                        const message = `L'utilisateur a été connecté avec succès`
-                        res.json({message, data: user})
-                    }
-                })
+        User.findOne({ where: {username : req.body.username}}).then(user => {
+            if(!user) {
+                const message = `L'utilisateur demandé n'existe pas.`
+                return res.status(404).json({message})
+            }
+            bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
+                if(!isPasswordValid) {
+                    const message = `Le mot de passe est incorrect.`
+                    return res.status(401).json({message, data: user})
+                } else {
+                    const message = `L'utilisateur a été connecté avec succès`
+                    return res.json({message, data: user})
+                }
             })
+        }).catch(error => {
+            const message = `L'utilisateur a pas pu être connecté. Réessayez plutard.`
+            res.status(500).json({message, data: error})
+        })
     })
 }
 
